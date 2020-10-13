abc2455a298d489f66888871098ec81eaaa59a70
SadMap <jjjvb3@gmail.com>
2020-10-13 03:17:27 -0700

    Api Entegrasyon Tamamlandı

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch')
+const sozluk = require("sozlukjs");
+
+sozluk.TDKDictionary.getMeaningData("sözlük1")
+    .then(data => console.log(data));
